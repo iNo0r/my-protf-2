6185199ecd6d25229ec6c9c7a56864d02352a3b8
King <pic.ner101@gmail.com>
2022-08-13 13:41:19 +0100

    svgs

---
+++ src/components/svgs/appLogo.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 67 67"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="app-logo"
+  >
+    <path
+      class="inner-letters"
+      d="M19.9868 45.1H29.2868L18.5468 26.53V46H17.6468V25H18.6968L29.9168 44.41V25H30.8168V46H19.9868V45.1ZM49.3483 46H48.4483V35.5H37.6483V46H36.7483V25H37.6483V34.6H48.4483V25H49.3483V46Z"
+      fill="#FB8181"
+    />
+    <rect
+      x="0.707107"
+      y="33.2893"
+      width="46.0782"
+      height="46.0782"
+      transform="rotate(-45 0.707107 33.2893)"
+      stroke="#FB8181"
+    />
+  </svg>
+</template>
+
+<style lang="scss">
+.app-logo:hover {
+  cursor: pointer;
+  .inner-letters {
+    transition: all 0.5s;
+    fill: white;
+  }
+}
+</style>
---
+++ src/components/svgs/arrowSvg.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 10 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M9 9L1 1.5V16.5L9 9Z" stroke="#CE4B4B" />
+  </svg>
+</template>
